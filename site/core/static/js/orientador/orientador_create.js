ea7b6c060829f66d248382c021d451c511300445
Rodrigo Oliveira <rodrigoodeoliveira.santos@gmail.com>
2021-10-22 23:34:34 -0300

    create orientador

--- site/core/static/js/orientador/orientador_create.js
+++ site/core/static/js/orientador/orientador_create.js
@@ -1,7 +1,17 @@
 document.querySelector("#create").addEventListener("click", () => {
     let data = {
-        "descricao": document.querySelector("textarea#desc").value,
-        "oficina": parseInt(id)
+        voluntario: document.querySelector("#voluntario").checked,
+        username: document.querySelector("#username").value,
+        nome: document.querySelector("#nome").value,
+        senha: document.querySelector("#senha").value,
+        cpf: document.querySelector("#cpf").value,
+        data_nasc: document.querySelector("#dtnasc").value,
+        endereco: document.querySelector("#endereco").value,
+        bairro: document.querySelector("#bairro").value,
+        cidade: document.querySelector("#cidade").value,
+        numero: document.querySelector("#numero").value,
+        uf: document.querySelector("#uf").value,
+        cep: document.querySelector("#cep").value
     }
 
     request_auth(`/api/orientador/create/`, "POST", data)
@@ -9,6 +19,8 @@ document.querySelector("#create").addEventListener("click", () => {
     .then(re => {
         if (re.success) {
             window.location.href = `/orientador/listagem/`
+        } else {
+            document.querySelector("#error").innerHTML = re.msg
         }
     })
 })  
