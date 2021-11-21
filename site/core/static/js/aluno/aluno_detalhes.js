40c22b98255bebd69327aeca70fff357f0524f92
Miguel Vieira Colombo <miguelhunter95@gmail.com>
2021-11-20 21:16:43 -0300

    Remoção dos styles

--- site/core/static/js/aluno/aluno_detalhes.js
+++ site/core/static/js/aluno/aluno_detalhes.js
@@ -24,13 +24,13 @@ const get_aluno = (id,edit) => {
             document.querySelector(".infos__datanasc").innerHTML = new Date(re.aluno.user__data_nasc).toLocaleDateString()
             document.querySelector(".infos__uf").innerHTML = re.aluno.user__uf
         } else {
-            document.querySelector(".infos__datanasc").innerHTML = `<input class="input" style="width: 320px;" type="date" value="`+re.aluno.user__data_nasc+`"  name="data_nasc">`
-            document.querySelector(".infos__cep").innerHTML = `<input type="number" style="width: 320px;" class="input" id="inputs" name="cep" onfocusout="buscarCep(event)" value="`+re.aluno.user__cep+`" required>`
-            document.querySelector(".infos__cidade").innerHTML = `<input type="text" style="width: 320px;" class="input" id="inputs" name="cidade" value="`+re.aluno.user__cidade+`" required>`
-            document.querySelector(".infos__logra").innerHTML = `<input type="text" style="width: 320px;" class="input" id="inputs" name="logradouro" value="`+re.aluno.user__endereco+`" required>`
-            document.querySelector(".infos__bairro").innerHTML = `<input type="text" style="width: 320px;" class="input" id="inputs" name="bairro" value="`+re.aluno.user__bairro+`" required>`
-            document.querySelector(".infos__num").innerHTML = `<input type="number" style="width: 320px;" class="input" id="inputs" name="numero" value="`+re.aluno.user__numero+`" required>`
-            document.querySelector(".infos__uf").innerHTML = `<input type="text" style="width: 320px;" class="input" id="inputs" name="uf" value="`+re.aluno.user__uf+`" required>`
+            document.querySelector(".infos__datanasc").innerHTML = `<input class="input" type="date" value="`+re.aluno.user__data_nasc+`"  name="data_nasc">`
+            document.querySelector(".infos__cep").innerHTML = `<input type="number" class="input" id="inputs" name="cep" onfocusout="buscarCep(event)" value="`+re.aluno.user__cep+`" required>`
+            document.querySelector(".infos__cidade").innerHTML = `<input type="text" class="input" id="inputs" name="cidade" value="`+re.aluno.user__cidade+`" required>`
+            document.querySelector(".infos__logra").innerHTML = `<input type="text" class="input" id="inputs" name="logradouro" value="`+re.aluno.user__endereco+`" required>`
+            document.querySelector(".infos__bairro").innerHTML = `<input type="text" class="input" id="inputs" name="bairro" value="`+re.aluno.user__bairro+`" required>`
+            document.querySelector(".infos__num").innerHTML = `<input type="number" class="input" id="inputs" name="numero" value="`+re.aluno.user__numero+`" required>`
+            document.querySelector(".infos__uf").innerHTML = `<input type="text" class="input" id="inputs" name="uf" value="`+re.aluno.user__uf+`" required>`
             document.querySelector(".edit_delet").style.display = "none"   
             document.querySelector(".cancel_save").style.display = "inline"
         }
